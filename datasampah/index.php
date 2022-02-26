6ed86cc584d415238bf8f268c792917656a416a3
abc <abc@example.com>
2022-02-26 11:31:16 +0700

    progres satu

---
+++ datasampah/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <?php require_once 'process.php' ; ?>
+
+    <?php 
+    
+    if (isset($_SESSION['message'])): ?>
+
+    <div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+        <?php 
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
+    </div>
+    <?php endif ?>
+
+    <div class="container">
+    <?php 
+        $mysqli = new mysqli('localhost','root','','taweb') or die($mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM sampah") or die($mysqli->error);
+       // pre_r(result);
+       ?>
+    <div class="row justify-content-center">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Jenis sampah</th>
+                    <th>satuan</th>
+                    <th>harga</th>
+                    <th>gambar</th>
+                    <th>deskripsi</th>
+                    <th colspan="2">Action</th>
+                </tr>
+            </thead>
+        <?php 
+            while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['jenis_sampah']; ?></td>
+                    <td><?php echo $row['satuan']; ?></td>
+                    <td><?php echo $row['harga']; ?></td>
+                    <td><?php echo $row['gambar']; ?></td>
+                    <td><?php echo $row['deskripsi']; ?></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>>"
+                            class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id']; ?>>"
+                            class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+        <?php endwhile; ?>
+
+        </table>
+    </div>
+
+    <?php 
+        function pre_r($array) {
+            echo '<pre>' ;
+            print_r($array);
+            echo '</pre>';
+        }
+    ?>
+
+    <div class="row justify-content-center">
+        <form action="process.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <div class="form-group">
+            <label>Jenis Sampah</label>
+            <input type="text" name="jenis_sampah" class="form-control" 
+                    value="<?php echo $jenis_sampah; ?>" placeholder="Enter jenis sampah">
+            </div>
+            <div class="form-group">
+            <label>Satuam</label>
+            <input type="text" name="satuan" class="form-control" 
+                    value="<?php echo $satuan; ?>" placeholder="Enter satuan">
+            </div>
+            <div class="form-group">
+            <label>Harga</label>
+            <input type="text" name="harga" class="form-control" 
+                    value="<?php echo $harga; ?>" placeholder="Enter harga">
+            </div>
+            <div class="form-group">
+            <label>Gambar</label>
+            <input type="text" name="gambar" class="form-control" 
+                    value="<?php echo $gambar; ?>" placeholder="Enter gambar">
+            </div>
+            <div class="form-group">
+            <label>Deskripsi</label>
+            <input type="text" name="deskripsi" class="form-control" 
+                    value="<?php echo $deskripsi; ?>"placeholder="Enter deskripsi">
+            </div>
+            <div class="form-group">
+            <?php 
+            if ($update == true);
+            ?>
+                <button type="submit" class="btn btn-info" name="update">Update</button>
+            
+                <button type="submit" class="btn btn-primary" name="save">Tambah Data</button>
+    
+            </div>
+        </form>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ datasampah/process.php
@@ -0,0 +1,71 @@
+<?php 
+
+session_start();
+
+$mysqli = new mysqli('localhost', 'root', '', 'taweb') or die (mysqli_error($mysqli));
+
+$id = 0;
+$update = false;
+$jenis_sampah = '';
+$satuan= '';
+$harga= '';
+$gambar= '';
+$deskripsi= '';
+
+if (isset($_POST['save'])){
+    $jenis_sampah = $_POST['jenis_sampah'];
+    $satuan = $_POST['satuan'];
+    $harga = $_POST['harga'];
+    $gambar = $_POST['gambar'];
+    $deskripsi = $_POST['deskripsi'];
+
+    $mysqli->query("INSERT INTO sampah (jenis_sampah, satuan, harga, gambar, deskripsi) 
+                    VALUES('$jenis_sampah', '$satuan', '$harga', '$gambar', '$deskripsi')") or
+        die($mysqli->error);
+
+    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['msg_type'] = "success";
+
+    header("location: index.php");
+}
+
+if (isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM sampah WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "danger";
+
+    header("location: index.php");
+}
+
+if (isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM sampah WHERE id=$id") or die($mysqli->error());
+    if (count($result)==1){
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+}   
+
+if (isset($_POST['update'])){
+    $id = $_POST['id'];
+    $jenis_sampah = $_POST['jenis_sampah'];
+    $satuan = $_POST['satuan'];
+    $harga = $_POST['harga'];
+    $gambar = $_POST['gambar'];
+    $deskripsi = $_POST['deskripsi'];
+
+    $mysqli->query("UPDATE data SET jenis_sampah='$jenis_sampah', satuan='$satuan', harga='$harga', gambar='$gambar', deskripsi='$deskripsi' WHERE id=$id") or
+            die($mysqli->error);
+
+    $SESSION['message'] = "Record has been updates!";
+    $SESSION['msg_type'] = "warning";
+
+    header('location: index.php');
+}
+
+
+?>
